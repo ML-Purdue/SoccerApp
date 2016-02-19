eb8981dc2c5c4572f67a1b0d4846a25fb3e9ede1
Michael A Roth <roth28@purdue.edu>
2016-02-19 15:15:19 -0500

    Added documentation fot the drawing strategy.

--- FootballSimulationApp/ISimulationDrawingStrategy.cs
+++ FootballSimulationApp/ISimulationDrawingStrategy.cs
@@ -3,8 +3,16 @@ using FootballSimulation;
 
 namespace FootballSimulationApp
 {
+    /// <summary>
+    ///     Encapsulates a drawing strategy for a football simulation.
+    /// </summary>
     internal interface ISimulationDrawingStrategy
     {
+        /// <summary>
+        ///     Draws the simulation with the specified graphics context.
+        /// </summary>
+        /// <param name="g">The graphics context.</param>
+        /// <param name="s">The simulation to draw.</param>
         void Draw(Graphics g, ISimulation s);
     }
 }
