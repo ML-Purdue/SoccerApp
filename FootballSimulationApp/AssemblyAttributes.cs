2c08751f88edd96d62c2c77b90cd94f86ee9d65c
Michael A Roth <roth28@purdue.edu>
2016-03-03 10:40:03 -0500

    Add missing file from last commit.

---
+++ FootballSimulationApp/AssemblyAttributes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Reflection;
+
+namespace FootballSimulationApp
+{
+    internal class AssemblyAttributes
+    {
+        public readonly string Company;
+        public readonly string Copyright;
+        public readonly string Description;
+        public readonly string Product;
+        public readonly string Title;
+        public readonly string Version;
+
+        public AssemblyAttributes(Assembly assembly)
+        {
+            Contract.Requires<ArgumentNullException>(assembly != null);
+
+            Title = GetAttribute<AssemblyTitleAttribute>(assembly, a => a.Title);
+            Version = assembly.GetName().Version.ToString();
+            Description = GetAttribute<AssemblyDescriptionAttribute>(assembly, a => a.Description);
+            Product = GetAttribute<AssemblyProductAttribute>(assembly, a => a.Product);
+            Copyright = GetAttribute<AssemblyCopyrightAttribute>(assembly, a => a.Copyright);
+            Company = GetAttribute<AssemblyCompanyAttribute>(assembly, a => a.Company);
+        }
+
+        private static string GetAttribute<T>(Assembly assembly, Func<T, string> func) where T : Attribute
+        {
+            var attributes = Attribute.GetCustomAttributes(assembly, typeof(T));
+            return attributes.Length == 0 ? string.Empty : func.Invoke((T)attributes[0]);
+        }
+    }
+}
