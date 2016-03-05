5224e3172c37de3aa693c41663e9434a47ef3772
Matthew T Page <page38@purdue.edu>
2016-03-05 16:09:25 -0500

    Created TestTeams.

--- FootballSimulationApp/TestTeams.cs
+++ FootballSimulationApp/TestTeams.cs
@@ -18,10 +18,6 @@ namespace FootballSimulationApp
 
     public class TeamA : Team
     {
-        public string Name => "Team Strategy A";
-        bool hasKicked = false;
-        int kickCounter = 0;
-
         public TeamA(ReadOnlyCollection<PointMass> players, RectangleF goalBounds) :
             base(players, goalBounds)
         {
@@ -29,33 +25,7 @@ namespace FootballSimulationApp
 
         public override Kick Execute(ISimulation simulation)
         {
-            //foreach (var p in team.Players)
-            Console.WriteLine(Players[0].Velocity.X);
-            Console.WriteLine(Players[0].Velocity.Y);
-            Console.WriteLine();
-            if (kickCounter < 34)
-            {
-                var p = Players[0];
-                var force = SteeringStrategies.Seek(p, Vector2.Zero, 50);
-                // var force = SteeringStrategies.Arrive(p, Vector2.Zero,p.MaxSpeed, 250);
-                p.Force = force;
-                if ((p.Position - Vector2.Zero).Length() < Math.Sqrt(p.Radius))
-                {
-                    kickCounter++;
-                    return new Kick(p, new Vector2(-1000, 0));
-                }
-
-            }
-            else
-            {
-                var p = Players[0];
-                var force = SteeringStrategies.SlowDown(p);
-
-                p.Force = force;
-            }
-            Console.WriteLine(kickCounter);
             return Kick.None;
         }
     }
 }
-
