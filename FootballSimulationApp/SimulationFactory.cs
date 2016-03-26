a3025c0e618aef78ac9aa2060fc16ea12a8e7976
Matthew T Page <page38@purdue.edu>
2016-03-26 15:42:53 -0400

    Change starting positions and start working on keep away strategy.

--- FootballSimulationApp/SimulationFactory.cs
+++ FootballSimulationApp/SimulationFactory.cs
@@ -24,16 +24,16 @@ namespace FootballSimulationApp
             for (var j = 0; j < 5; j++)
             {
                 team1Players[j] = new PointMass(mass, radius, maxForce, maxSpeed,
-                    new Vector2(-w / 4 + w / 2, -h / 4 + j * h / 8), Vector2.Zero);
+                    new Vector2(-w / 4 + w / 2 + (j % 2 != 0 ? 100 : 0), -h / 4 + j * h / 8), Vector2.Zero);
                 team2Players[j] = new PointMass(mass, radius, maxForce, maxSpeed,
-                    new Vector2(-w / 4, -h / 4 + j * h / 8), Vector2.Zero);
+                    new Vector2(-w / 4 + (j % 2 != 0 ? -100 : 0), -h / 4 + j * h / 8), Vector2.Zero);
             }
 
             var pitch = new RectangleF(-w / 2, -h / 2, w, h);
             var team1Goal = new RectangleF(-w / 2 - goalW, -goalH / 2, goalW, goalH);
             var team2Goal = new RectangleF(w / 2, -goalH / 2, goalW, goalH);
-            var team1 = new TeamA(new ReadOnlyCollection<PointMass>(team1Players), team1Goal);
-            var team2 = new NullTeam(new ReadOnlyCollection<PointMass>(team2Players), team2Goal);
+            var team1 = new KeepawayTeam(new ReadOnlyCollection<PointMass>(team1Players), team1Goal);
+            var team2 = new KeepawayTeam(new ReadOnlyCollection<PointMass>(team2Players), team2Goal);
             var ball = new PointMass(1, 2.5f, 1000, 100, Vector2.Zero, Vector2.Zero);
 
             return new Simulation(new ReadOnlyCollection<Team>(new Team[] { team1, team2 }), ball, pitch, 3);
--- FootballSimulationApp/TestTeams.cs
+++ FootballSimulationApp/TestTeams.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.ObjectModel;
 using System.Drawing;
 using FootballSimulation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System;
 
 namespace FootballSimulationApp
 {
@@ -17,15 +21,52 @@ namespace FootballSimulationApp
         }
     }
 
-    internal class TeamA : Team
+    internal class KeepawayTeam : Team
     {
-        public TeamA(ReadOnlyCollection<PointMass> players, RectangleF goalBounds) :
+        private const int EdgeOverlapRadius = 100;
+        private const int PlayerOverlapRadius = 150;
+        private const float StoppingMultiplier = -1f;
+
+        public KeepawayTeam(ReadOnlyCollection<PointMass> players, RectangleF goalBounds) :
             base(players, goalBounds)
         {
         }
 
         public override Kick Execute(ISimulation simulation)
         {
+            var allPlayers = simulation.Teams[0].Players.Concat(simulation.Teams[1].Players);
+
+            foreach (var player in Players)
+            {
+                Vector2 v = new Vector2();
+
+                foreach (var otherPlayer in allPlayers)
+                {
+                    if (player == otherPlayer)
+                        continue;
+
+                    var between = player.Position - otherPlayer.Position;
+                    if (between.Length() < PlayerOverlapRadius)
+                        v += Vector2.Normalize(between);
+                }
+
+                var force = Vector2.Normalize(v) * 10;
+                var stoppingForce = StoppingMultiplier * player.Velocity;
+
+                force = v.Length() > 0 ? force :stoppingForce;
+
+                if (Math.Abs(player.Position.X - simulation.PitchBounds.X) < EdgeOverlapRadius && player.Velocity.X < 0)
+                    force = new Vector2(StoppingMultiplier * player.Velocity.X, force.Y);
+                if (Math.Abs(player.Position.X - simulation.PitchBounds.Right) < EdgeOverlapRadius && player.Velocity.X > 0)
+                    force = new Vector2(StoppingMultiplier * player.Velocity.X, force.Y);
+                if (Math.Abs(player.Position.Y - simulation.PitchBounds.Y) < EdgeOverlapRadius && player.Velocity.Y < 0)
+                    force = new Vector2(force.X, StoppingMultiplier * player.Velocity.Y);
+                if (Math.Abs(player.Position.Y - simulation.PitchBounds.Bottom) < EdgeOverlapRadius && player.Velocity.Y > 0)
+                    force = new Vector2(force.X, StoppingMultiplier * player.Velocity.Y);
+
+                player.Force = force;
+            }
+
             return Kick.None;
         }
     }
